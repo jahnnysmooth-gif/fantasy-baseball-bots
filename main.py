b3228dd6a0f9645f57e40142155dc4c35e760d8e
Jonathan Ramlall <jonathanramlall@MacBook-Pro.local>
2026-03-15 10:16:56 -0400

    Remove RSS bot from main worker and keep blueprint service

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import asyncio
 
 from player_profiles_bot import start_player_profiles_bot
 from closer_bot import start_closer_bot
-from mlb_rss_news_bot import start_mlb_rss_news_bot
+from injury_bot import start_injury_bot
 
 
 async def main():
     await asyncio.gather(
         start_player_profiles_bot(),
         start_closer_bot(),
-        asyncio.to_thread(start_mlb_rss_news_bot),
+        start_injury_bot(),
     )
 
 
