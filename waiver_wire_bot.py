b2a2dbb01806f487aa7c995d3b895576aeafa4f8
Jonathan Ramlall <jonathanramlall@MacBook-Pro.local>
2026-04-04 20:24:08 -0400

    Fix ESPN filter: sort required with limit

--- waiver_wire_bot.py
+++ waiver_wire_bot.py
@@ -73,6 +73,7 @@ async def fetch_espn_ownership():
     headers = {
         'User-Agent': 'Mozilla/5.0',
         'Accept': 'application/json',
+        'x-fantasy-filter': '{"players":{"limit":300,"sortPercentChange":{"sortPriority":1,"sortAsc":false},"filterActive":{"value":true}}}',
     }
 
     async with aiohttp.ClientSession() as session:
